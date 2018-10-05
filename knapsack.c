bc7153f739f502707c85cf85b2ce05406e93fd40
Keivn France <kfrance@mail.umw.edu>
2018-10-04 21:27:39 -0400

    added test files, adding some file reading functionality, putchar works. need to store putchar.

--- knapsack.c
+++ knapsack.c
@@ -22,27 +22,40 @@ void* func(void* idp) {
 }
 
 int main (int argc, char** argv) {
-
+    char *filename;
+    int nums[30] = {0};
+    int x, y = 0;
     /* get the number of threads */
     if (argc < 3 || argv[2] < 0) {
         printf("Pass the knapsack file name and the number of threads to run the program with!\n");
         return 0;
     } else { 
-        char *filename;
-
         filename = argv[1];
         printf("filename: %s\n", filename);
-
+         
         num_threads = atoi(argv[2]);
         printf("threads: %d\n", num_threads);
     }
 
     /* read in file */
-    FILE *fopen(const char *filename, const char * r);
-
-    /* close file */
-    int fclose( FILE *filename );
+    FILE *file = fopen(filename, "r");
     
+    if (file == NULL){
+        printf("Error! opening file");
+        // Program exits if the file pointer returns NULL.
+        exit(1);
+    }
+
+    while ((x = getc(file)) != EOF) {
+        putchar(x);
+        nums[y] = x;
+        //printf("\nnums[%d] = %d\n", y, x);
+        y = y + 1;
+    }
+
+    /* close the file */    
+    fclose(file);
+   
     /* an array of threads */
     pthread_t threads[num_threads];
     int ids[num_threads];
@@ -51,19 +64,19 @@ int main (int argc, char** argv) {
     /* spawn all threads */
     for (i = 0; i < num_threads; i++) {
         ids[i] = i;
-        pthread_create(&threads[i], NULL, sum_part, &ids[i]);
+        //pthread_create(&threads[i], NULL, i, &ids[i]);
     }
 
     /* join all threads collecting answer */
     int answer = 0;
     for (i = 0; i < num_threads; i++) {
         int* partial;
-        pthread_join(threads[i], (void**) &partial);
-        answer += *partial;
-        free(partial);
+        //pthread_join(threads[i], (void**) &partial);
+        //answer += *partial;
+        //free(partial);
     }
 
     /* now all results are in */
-    printf("Final answer = %d.\n", answer);
+    //printf("Final answer = %d.\n", answer);
     pthread_exit(NULL);
 }
