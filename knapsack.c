9dd3fc5b5c6c2a5b9813d083a7d363d9637372e3
Keivn France <kfrance@mail.umw.edu>
2018-10-09 10:27:05 -0400

    completed possibility matrix

--- knapsack.c
+++ knapsack.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <math.h>
 
 struct item {
     int weight;
@@ -20,10 +21,10 @@ struct item items[50];
 void* func(void* idp) {
     /* get our thread id */
     int id = * (int*) idp;
-    
+
     int possibilities = 2 ^ item_count;
     int amtPerThread = possibilities / num_threads;
-    
+
 
     /* calculate the best combination based on file */
     //printf("item 1:%d\n", items[1].value);
@@ -37,33 +38,42 @@ void* func(void* idp) {
     return answer;
 }
 
+void add1(int *a, int len) {
+    int carry = 1;
+    for (int i = len - 1; carry > 0 && i >= 0; i--) {
+        int result = a[i] + carry;
+        carry = result >> 1;
+        a[i] = result & 1;
+    }
+}
+
 int main (int argc, char** argv) {
     char *filename;
     int x = 0;
     int y = 0;
-    
+
     /* get the number of threads */
     if (argc < 3 || argv[2] < 0) {
         printf("Pass the knapsack file name and the number of threads to run the program with!\n");
         return 0;
     } else { 
         filename = argv[1];
-        printf("filename: %s\n", filename);
-         
+        //printf("filename: %s\n", filename);
+
         num_threads = atoi(argv[2]);
-        printf("threads: %d\n", num_threads);
+        //printf("threads: %d\n", num_threads);
     }
 
     /* read in file */
     FILE *file = fopen(filename, "r");
-    
+
     /* catches file name error */
     if (file == NULL){
         printf("Error! Incorrect file name.\n");
         // Program exits if the file pointer returns NULL.
         exit(1);
     }
-    
+
     /* reads numbers from file, keeps track of number of items, stores item values in array of structs */
     fscanf(file, "%d", &weight_limit);
     while (fscanf(file, "%d %d", &items[y].weight, &items[y].value) != EOF) {
@@ -77,22 +87,38 @@ int main (int argc, char** argv) {
 
     /* close the file */    
     fclose(file);
-   
+
     /* an array of threads */
     pthread_t threads[num_threads];
     int ids[num_threads];
-    int i, j;
-    int total_combos = 2 ^ item_count;
+    int i, j, temp;
+    int total_combos = pow(2, item_count);
+    //printf("%d",total_combos); 
     int amt_per_thread = total_combos / num_threads;    
-    int possibilities[total_combos][item_count];
+    int possibilities[(int)total_combos][item_count];
+    int line[item_count];
+    
+    for(i = 0; i < total_combos; i++) {
+        for(j = 0; j < item_count; j++) {
+            possibilities[i][j] = 0;
+        }
+        line[i] = 0;
+    }
 
     /* set possibilites matrix */
-    for(i = 0; i <= total_combos; i++) {
-        for(j = 0; j <= item_count; j++) { 
-            possibilities[i][j] = 1;
-            printf("possibilities[%d][%d] = %d\n", i, j, possibilities[i][j]);
+    for(i = 0; i < total_combos; i++) {
+        for(j = 0; j < item_count ; j++) { 
+            //here
+            add1(line, item_count);
+            //printf("line[%d]: %d\n", j, line[j]);
+            possibilities[i][j] = line[j];
+            printf("[%d][%d] = %d ", i, j, possibilities[i][j]);
         }
+        //printf("line[%d]: %d\n", i, line[j]);
+        possibilities[i][j] = line[i];
+        printf("\n");
     }
+
     /* spawn all threads */
     for (i = 0; i < num_threads; i++) {
         for(j = 0; j < amt_per_thread; j++) {
